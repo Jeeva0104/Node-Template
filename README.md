c0e3532c75966a5596317d36f698a0942ab22a57
Jeeva <jeeva@tricog.com>
2019-02-21 11:51:16 +0530

    created docker

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Docker
+# small setup of server using docker
---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const os = require('os');
+
+const PORT = 8000
+const app = express()
+app.get('/', (req, res) => {
+    console.log(' reached here ! ');
+    res.send(`Final test Jenkins CI/CD, Docker Registry, Rancher Compose CLI, Rancher -> Jenkins Notification, I'm run on ${os.hostname()}`)
+})
+app.listen(PORT)
+console.log(`Running on http://0.0.0.0:${PORT}`)
