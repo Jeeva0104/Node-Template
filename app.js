e0383630119b386526a29d46697dc8b8d614c60f
Jeeva <jeeva@tricog.com>
2019-12-31 13:12:21 +0530

    learn git

--- app.js
+++ app.js
@@ -8,4 +8,4 @@ app.get('/', (req, res) => {
     res.send(`Final test Jenkins CI/CD, Docker Registry, Rancher Compose CLI, Rancher -> Jenkins Notification, I'm run on ${os.hostname()}`)
 })
 app.listen(PORT)
-console.log(`Running on http://0.0.0.0:${PORT}`)
+console.log(`Running on http://0.0.0.0:${PORT}`);
