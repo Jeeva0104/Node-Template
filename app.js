ab73aeeae4d0b1c46643832535b292e3d1b8fbde
Ramachandran <jramachandran@NTS205734MC.local>
2020-03-28 17:10:11 +0530

    add timeout

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
-const express = require('express')
-const os = require('os');
+const express = require("express");
+const os = require("os");
 
-const PORT = 8000
-const app = express()
-app.get('/', (req, res) => {
-    console.log(' reached here ! ');
-    res.send(`Final test Jenkins CI/CD, Docker Registry, Rancher Compose CLI, Rancher -> Jenkins Notification, I'm run on ${os.hostname()}`)
-})
-app.listen(PORT)
-console.log(`Running on http://0.0.0.0:${PORT}`)
+const PORT = 8000;
+const app = express();
+app.get("/", async (req, res) => {
+  await resolveAfterXSeconds();
+  console.log(" reached here ! ");
+  return res.send(
+    `Final test Jenkins CI/CD, Docker Registry, Rancher Compose CLI, Rancher -> Jenkins Notification, I'm run on ${os.hostname()}`
+  );
+});
+function resolveAfterXSeconds() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve("resolved");
+    }, 60 * 60 * 60);
+  });
+}
+app.listen(PORT);
+console.log(`Running on http://0.0.0.0:${PORT}`);
